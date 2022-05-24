0bd1d727b2fa868dd86df6834a43f48b7db2400b
Ruban Mikhail <m.ruban@hh.ru>
2022-05-25 00:42:06 +0300

    add api request

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
+import axios from 'axios';
 import express from 'express';
 import fs from 'fs';
 import { createMemoryHistory } from 'history';
 import path from 'path';
 import serialize from 'serialize-javascript';
 
+import counter from 'models/counter';
 import { createStore } from 'models/store';
 
 import { App } from 'components/App';
@@ -21,9 +23,21 @@ app.get(/\.(json)$/, async (req, res) => {
     return res.send({ test: 1 });
 });
 
+const auth = {
+    username: 'getrand',
+    password: 'secret',
+};
+
 app.use('*', async (req, res) => {
+    let api;
+    try {
+        api = await axios.get('http://gamespirit.org/api/v1/', { auth });
+    } catch (error) {
+        res.status(500);
+        return res.send('api error');
+    }
     const history = createMemoryHistory({ initialEntries: [req.originalUrl], initialIndex: 0 });
-    const store = createStore(history);
+    const store = createStore(history, { counter: { value: api.data.count } });
     const appView = renderToString(
         <Provider store={store}>
             <App history={history} />
