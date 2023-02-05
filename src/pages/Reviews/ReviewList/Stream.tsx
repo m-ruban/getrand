79457d2025d9167d54f8416e420b3a8111e28aec
Ruban Mikhail <m.ruban@hh.ru>
2023-02-05 16:24:05 +0300

    add review streams

---
+++ src/pages/Reviews/ReviewList/Stream.tsx
@@ -0,0 +1,108 @@
+import React, { FC, Fragment, useCallback, useEffect, useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import axios from 'axios';
+
+import { ImageLoad } from 'gg-ukit/components/Image';
+
+import { ArticleSearch } from 'models/lastReviews';
+import { setPagination } from 'models/pagination';
+
+import elementObserver from 'modules/elementObserver';
+
+import ArticleDivider from 'components/ArticleDivider';
+import ArticleSearchItem from 'components/ArticleSearchItem';
+
+interface StreamProps {
+    defaultList: ArticleSearch[];
+    page: number;
+    disableLoadStreams: () => void;
+}
+
+const LIMIT_ITEMS = 10;
+
+const Stream: FC<StreamProps> = ({ defaultList, page, disableLoadStreams }) => {
+    const dispatch = useDispatch();
+    const ids = useRef<number[]>(defaultList.map(({ id }) => id));
+    const streamRef = useRef<HTMLDivElement>(null);
+    const [showStream, setShowStream] = useState<boolean>(true);
+    const [list, setList] = useState<ArticleSearch[]>(defaultList);
+
+    // set empty list and save height
+    const onHideStream = useCallback(() => {
+        streamRef.current.style.height = `${streamRef.current.offsetHeight}px`;
+        setShowStream(false);
+        setList([]);
+    }, []);
+
+    // load list by ids
+    const onShowStream = useCallback(() => {
+        if (showStream) {
+            return;
+        }
+        const params = ids.current.map((id) => 'id=' + id).join('&');
+        axios.get(`/shards/reviews-by-ids/?${params}`).then((response) => {
+            const { articles } = response.data;
+            setList(articles);
+            setShowStream(true);
+            streamRef.current.style.height = null;
+        });
+    }, [showStream]);
+
+    // load data by page
+    useEffect(() => {
+        if (defaultList.length > 0) {
+            return;
+        }
+        axios.get(`/shards/reviews/?page=${page}`).then((response) => {
+            const { articles, pagination } = response.data;
+            // save ids for loading
+            ids.current = articles.map(({ id }) => id);
+            setList(articles);
+            // paginations need recalc (because change curr page)
+            dispatch(setPagination(pagination));
+            if (articles.length < LIMIT_ITEMS) {
+                // detect end of section
+                disableLoadStreams();
+            }
+        });
+    }, [defaultList, page, disableLoadStreams, dispatch]);
+
+    // set observers
+    useEffect(() => {
+        if (!streamRef.current) {
+            return;
+        }
+        const streamObserver = elementObserver({
+            element: streamRef.current,
+            onShow: onShowStream,
+            onHide: onHideStream,
+            rootMargin: '500px 0px 500px 0px',
+        });
+        return () => {
+            // because callbacs recals
+            streamObserver.stopObserverving();
+        };
+    }, [onHideStream, onShowStream]);
+
+    return (
+        <div ref={streamRef} className="stream">
+            {showStream &&
+                list.map(({ id, ...props }, index) => {
+                    return (
+                        <Fragment key={id}>
+                            <ArticleSearchItem
+                                type="review"
+                                id={id}
+                                {...props}
+                                loading={index < 3 ? ImageLoad.Eager : ImageLoad.Lazy}
+                                short
+                            />
+                            {index !== list.length - 1 && <ArticleDivider xsOnly />}
+                        </Fragment>
+                    );
+                })}
+        </div>
+    );
+};
+
+export default Stream;
--- src/pages/Reviews/ReviewList/index.tsx
+++ src/pages/Reviews/ReviewList/index.tsx
@@ -1,35 +1,69 @@
-import React, { FC, Fragment } from 'react';
+import React, { FC, useCallback, useEffect, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 
-import { ImageLoad } from 'gg-ukit/components/Image';
-
+// import axios from 'axios';
+// import { ArticleSearch } from 'models/lastReviews';
 import { RootStore } from 'models/reducers';
 
-import ArticleDivider from 'components/ArticleDivider';
-import ArticleSearchItem from 'components/ArticleSearchItem';
+import elementObserver from 'modules/elementObserver';
+
 import ColumnContainer from 'components/ColumnContainer';
+import Loader from 'components/Loader';
 import SectionHeader from 'components/SectionHeader';
+
+import Stream from 'pages/Reviews/ReviewList/Stream';
+
+const DEFAULT_LIST = [];
 
 const ReviewList: FC = () => {
     const reviewList = useSelector((state: RootStore) => state.reviews);
+    const defaultPage = useSelector((state: RootStore) => state.matchedParams.page || '1');
+
+    const loaderRef = useRef<HTMLDivElement>(null);
+    const [pages, setPages] = useState<number[]>([Number(defaultPage)]);
+    const [enableLoadStreams, setEnableLoadStreams] = useState<boolean>(true);
+
+    const disableLoadStreams = useCallback(() => {
+        setEnableLoadStreams(false);
+    }, []);
+
+    const onShowLoader = useCallback(() => {
+        setPages((pages) => {
+            const last = pages[pages.length - 1];
+            return [...pages, last + 1];
+        });
+    }, []);
+
+    useEffect(() => {
+        if (!loaderRef.current || !enableLoadStreams) {
+            return;
+        }
+        const loaderObserver = elementObserver({
+            element: loaderRef.current,
+            onShow: onShowLoader,
+            rootMargin: '0px 0px 500px 0px',
+        });
+        return () => {
+            // because loader down to bottom
+            loaderObserver.stopObserverving();
+        };
+    }, [onShowLoader, enableLoadStreams]);
+
     return (
         <>
             <SectionHeader title="Обзоры игр" combineColor />
             <ColumnContainer>
-                {reviewList.map(({ id, ...props }, index) => {
+                {pages.map((page, index) => {
                     return (
-                        <Fragment key={id}>
-                            <ArticleSearchItem
-                                type="review"
-                                id={id}
-                                {...props}
-                                loading={index < 3 ? ImageLoad.Eager : ImageLoad.Lazy}
-                                short
-                            />
-                            {index !== reviewList.length - 1 && <ArticleDivider xsOnly />}
-                        </Fragment>
+                        <Stream
+                            key={page}
+                            page={page}
+                            defaultList={index === 0 ? reviewList : DEFAULT_LIST}
+                            disableLoadStreams={disableLoadStreams}
+                        />
                     );
                 })}
+                <div ref={loaderRef}>{enableLoadStreams && <Loader />}</div>
             </ColumnContainer>
         </>
     );
