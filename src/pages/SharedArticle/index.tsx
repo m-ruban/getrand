8745b419b4415a3c343d34bcac2eea86253da55a
Ruban Mikhail <m.ruban@hh.ru>
2025-03-09 20:02:38 +0100

    clear section into shared article

--- src/pages/SharedArticle/index.tsx
+++ src/pages/SharedArticle/index.tsx
@@ -7,26 +7,20 @@ import { RootStore } from 'models/reducers';
 
 import { ImageLoadContext } from 'hooks/useImageLoadType';
 
-import Authors from 'components/ArticleView/Authors';
 import RelatedArticles from 'components/ArticleView/RelatedArticles';
-import Keywords from 'components/Keywords';
 import LitePageLayout from 'components/LitePageLayout';
 import TreeElement from 'components/TreeElement';
 
 import './sharedArticle.less';
 
 const SharedArticle: FC = () => {
-    const { seo, redactor, autor, children, article_type_keyword } = useSelector((state: RootStore) => state.article);
+    const { seo, children, article_type_keyword } = useSelector((state: RootStore) => state.article);
     return (
         <ImageLoadContext.Provider value={ImageLoad.Eager}>
             <LitePageLayout>
                 <div>
                     <TreeElement treeElement={seo.render_tree} />
                     <RelatedArticles articles={children} type={article_type_keyword} />
-                    <div>
-                        <Authors redactor={redactor} author={autor} />
-                        <Keywords keywords={seo.meta_keyword} />
-                    </div>
                 </div>
             </LitePageLayout>
         </ImageLoadContext.Provider>
